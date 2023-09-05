24c6a50ff6ca1f25080964f14e5223f01937292c
Shudhanshu26 <110622295+Shudhanshu26@users.noreply.github.com>
2023-09-05 18:35:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const celsiusInput = document.getElementById('celsius');
+    const fahrenheitOutput = document.getElementById('fahrenheit');
+    const convertBtn = document.getElementById('convertBtn');
+
+    convertBtn.addEventListener('click', function() {
+        const celsiusValue = parseFloat(celsiusInput.value);
+        if (!isNaN(celsiusValue)) {
+            const fahrenheitValue = (celsiusValue * 9/5) + 32;
+            fahrenheitOutput.textContent = fahrenheitValue.toFixed(2) + " °F";
+        } else {
+            fahrenheitOutput.textContent = "Invalid input";
+        }
+    });
+});
